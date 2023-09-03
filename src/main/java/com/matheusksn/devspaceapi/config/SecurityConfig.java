0d7c5797a0ed3286a98ffafaca4fa214fd938a42
Matheus <101477339+matheusksn@users.noreply.github.com>
2023-09-02 21:03:32 -0300

    auth

--- src/main/java/com/matheusksn/devspaceapi/config/SecurityConfig.java
+++ src/main/java/com/matheusksn/devspaceapi/config/SecurityConfig.java
@@ -7,10 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig {
 
     @Bean
